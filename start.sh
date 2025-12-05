a31eec9ff660759f8dc18e84df5d2a8cb748f30b
karsten tschiersch <provoware.one@gmail.com>
2025-12-05 23:03:31 +0100

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,3 @@
+# Startsetup Routine mit: prüfen und oder erstellen einer virtuellen umgebung, öffnen dieser Umgebunmg
+# Vollautomatische Prüfung und Auflösung aller Abhängigkeiten und Installationen mit perfektem detailierten nutzerfeedback über fortschritt, status und sonstige hilfreiche infos
+# abschließend vollautomatischer Toolstart in der virtuellen umgebung und GUI start
